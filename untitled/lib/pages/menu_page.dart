0eab54f28330181bcaf68c98febb834eb643d56c
Mohamed-Salmony <mohamedsamysalmony@gmail.com>
2023-12-30 11:25:51 +0200

    updates BookMarks Page

--- untitled/lib/pages/menu_page.dart
+++ untitled/lib/pages/menu_page.dart
@@ -189,7 +189,7 @@ class _MenuPageState extends State<MenuPage> {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => ChatListPage()),
+                                  builder: (context) => BookmarksPage()),
                             );
                           },
                         ),
